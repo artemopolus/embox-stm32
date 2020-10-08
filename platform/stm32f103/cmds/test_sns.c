67d234b48d3f4a1805e78b88f836a668a29b6b03
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2020-10-08 09:39:53 +0300

    add ism330 test fun

--- platform/stm32f103/cmds/test_sns.c
+++ platform/stm32f103/cmds/test_sns.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 #include "lsm303ah/apollon_lsm303ah_spi_generated.h"
+#include "ism330dlc/apollon_ism330dlc_spi_generated.h"
 
 int main(int argc, char *argv[]) {
 
@@ -39,12 +40,30 @@ int main(int argc, char *argv[]) {
 	{
 		printf("whoami xl test failed!");
 	}
-	
+
+	res = 0;
+
+	apollon_ism330dlc_spi_set_option(0x12, 0x0C);
+	value_xl = apollon_ism330dlc_spi_get_option(0x0F);
+	printf("v= %d. try next data!\n", value_xl);
+	value_xl = apollon_ism330dlc_spi_get_option(0x0F);
+	printf("v= %d. try next data!\n", value_xl);
+	value_xl = apollon_ism330dlc_spi_get_option(0x0F);
+	printf("v= %d. try next data!\n", value_xl);
+	if (value_xl == 0x6A)
+	{
+		printf("whoami test for ism330dlc done!");
+	}
 	if (res)
 	{
 		printf("All done right!\n");
 		res = 0;
 	}
+	else
+	{
+		printf("something gone wrong!\n");
+	}
+	
 
 	return res;
 }
---
+++ platform/stm32f103/ism330dlc/apollon_ism330dlc_spi_generated.c
@@ -0,0 +1,70 @@
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_ll_dma.h"
+#include "stm32f1xx_ll_spi.h"
+#include "stm32f1xx_ll_rcc.h"
+#include "stm32f1xx_ll_bus.h"
+#include "stm32f1xx_ll_system.h"
+#include "stm32f1xx_ll_exti.h"
+#include "stm32f1xx_ll_cortex.h"
+#include "stm32f1xx_ll_utils.h"
+#include "stm32f1xx_ll_pwr.h"
+#include "stm32f1xx.h"
+#include "stm32f1xx_ll_gpio.h"
+
+#include "apollon_ism330dlc_spi_generated.h"
+#include <embox/unit.h>
+#include <kernel/printk.h>
+
+struct apollon_ism330dlc_spi_dev {
+	int spi_bus;
+	struct spi_device *spi_dev;
+};
+
+struct apollon_ism330dlc_spi_dev apollon_ism330dlc_spi_dev0 = {
+  .spi_bus = 0,
+};
+
+EMBOX_UNIT_INIT(apollon_ism330dlc_init);
+static int apollon_ism330dlc_spi_init( struct apollon_ism330dlc_spi_dev *dev )
+{
+  LL_APB2_GRP1_EnableClock( ISM330DLC_CLC);
+  LL_GPIO_SetPinMode(       ISM330DLC_CS_GPIO_Port, ISM330DLC_CS_Pin, LL_GPIO_MODE_OUTPUT);
+
+    return 0;
+}
+static int apollon_ism330dlc_init(void)
+{
+  struct apollon_ism330dlc_spi_dev *dev = &apollon_ism330dlc_spi_dev0;
+  apollon_ism330dlc_spi_init(dev);
+  return 0;
+}
+uint8_t apollon_ism330dlc_spi_get_option(uint8_t address)
+{
+    uint8_t value = address | 0x80; //10000000b
+	LL_GPIO_ResetOutputPin(ISM330DLC_CS_GPIO_Port, ISM330DLC_CS_Pin);
+	while(!LL_SPI_IsActiveFlag_TXE( ISM330DLC_SPI));
+	LL_SPI_TransmitData8(           ISM330DLC_SPI, value);
+	while(!LL_SPI_IsActiveFlag_TXE( ISM330DLC_SPI));
+	while(LL_SPI_IsActiveFlag_BSY(  ISM330DLC_SPI));
+	LL_SPI_SetTransferDirection(    ISM330DLC_SPI,LL_SPI_HALF_DUPLEX_RX);
+	while(!LL_SPI_IsActiveFlag_RXNE(ISM330DLC_SPI));
+	uint8_t result = 0;
+	result = LL_SPI_ReceiveData8(   ISM330DLC_SPI);
+	LL_SPI_SetTransferDirection(    ISM330DLC_SPI,LL_SPI_HALF_DUPLEX_TX);
+	LL_GPIO_SetOutputPin(ISM330DLC_CS_GPIO_Port, ISM330DLC_CS_Pin);
+    return result;
+}
+uint8_t apollon_ism330dlc_spi_set_option(uint8_t address, uint8_t value)
+{
+    uint8_t mask = 0x7F ;//01111111b
+	mask &= address;
+    LL_GPIO_ResetOutputPin(ISM330DLC_CS_GPIO_Port, ISM330DLC_CS_Pin);
+    while(!LL_SPI_IsActiveFlag_TXE( ISM330DLC_SPI));
+    LL_SPI_TransmitData8(           ISM330DLC_SPI, mask);
+    while(!LL_SPI_IsActiveFlag_TXE( ISM330DLC_SPI));
+	LL_SPI_TransmitData8(           ISM330DLC_SPI, value);
+	while(!LL_SPI_IsActiveFlag_TXE( ISM330DLC_SPI));
+	while(LL_SPI_IsActiveFlag_BSY(  ISM330DLC_SPI));
+	LL_GPIO_SetOutputPin(ISM330DLC_CS_GPIO_Port, ISM330DLC_CS_Pin);
+    return 0;
+}
