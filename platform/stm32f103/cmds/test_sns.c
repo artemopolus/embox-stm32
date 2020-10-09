e13ff622a7ec36c2f44e2cb8f10680e8b9a93393
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2020-10-09 09:27:34 +0300

    some changes for both lsm303 and ism330 tests

--- platform/stm32f103/cmds/test_sns.c
+++ platform/stm32f103/cmds/test_sns.c
@@ -14,19 +14,17 @@ int main(int argc, char *argv[]) {
 	//write_lsm303ah(0x21,0x05);
 	apollon_lsm303ah_spi_set_option(0x21, 0x05);
 	// value_xl = read_lsm303ah(0x0f);
-	value_xl = apollon_lsm303ah_spi_get_option(0x0f);
-	printf("v= %d. try next data!\n", value_xl);
-	value_xl = apollon_lsm303ah_spi_get_option(0x0f);
-	printf("v= %d. try next data!\n", value_xl);
-	value_xl = apollon_lsm303ah_spi_get_option(0x0f);
-	printf("v= %d. try next data!\n", value_xl);
-	value_xl = apollon_lsm303ah_spi_get_option(0x0f);
-	printf("v= %d. try next data!\n", value_xl);
-	value_xl = apollon_lsm303ah_spi_get_option(0x0f);
-	printf("v= %d. try next data!\n", value_xl);
+	uint8_t address_lsm303ah = 0x0f;
+	for (uint8_t i = 0; i < 5; i++)
+	{
+		/* code */
+		value_xl = apollon_lsm303ah_spi_get_option(address_lsm303ah);
+		printf("Get value from %#04x = %d ( %#04x )\n", address_lsm303ah, value_xl, value_xl);
+	}
+	
 	if (value_xl == 0x43)
 	{
-		printf("whoami xl test success!");
+		printf("whoami xl test success!\n");
 		// value_mg = read_lsm303ah(0x4f);
 		value_mg = apollon_lsm303ah_spi_get_option(0x4f);
  		if(0x40 == value_mg)      res = 1;
@@ -48,15 +46,19 @@ int main(int argc, char *argv[]) {
 	res = 0;
 
 	apollon_ism330dlc_spi_set_option(0x12, 0x0C);
-	value_xl = apollon_ism330dlc_spi_get_option(0x0F);
-	printf("v= %#04x. try next data!\n", value_xl);
-	value_xl = apollon_ism330dlc_spi_get_option(0x0F);
-	printf("v= %#04x. try next data!\n", value_xl);
-	value_xl = apollon_ism330dlc_spi_get_option(0x0F);
-	printf("v= %d. try next data!\n", value_xl);
+	
+	uint8_t address_ism330dlc = 0x0f;	
+	for (uint8_t i = 0; i < 5; i++)
+	{
+		value_xl = apollon_ism330dlc_spi_get_option(address_ism330dlc);
+		printf("Get value from %#04x = %d ( %#04x )\n", address_ism330dlc, value_xl, value_xl);
+
+	}
+	
+	
 	if (value_xl == 0x6A)
 	{
-		printf("whoami test for ism330dlc done!");
+		printf("whoami test for ism330dlc done!\n");
 	}
 	if (res)
 	{
