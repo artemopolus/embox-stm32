64af9c36f2c8eb52f6d2e96fbb69bd14cfacf2a8
Artem <artem.o.kuznecov@gmail.com>
2020-10-18 18:37:16 +0300

    add spi implent

--- platform/stm32f103/cmds/test_sns.c
+++ platform/stm32f103/cmds/test_sns.c
@@ -5,6 +5,7 @@
 #include "lsm303ah/apollon_lsm303ah_spi_generated.h"
 #include "ism330dlc/apollon_ism330dlc_spi_generated.h"
 #include "bmp280/apollon_bmp280_i2c_generated.h"
+#include "spi/apollon_spi2_generated.h"
 
 int main(int argc, char *argv[]) {
 
