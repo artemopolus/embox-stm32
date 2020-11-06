91487206161a890b956fade10347dd376b699a42
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2020-11-06 17:21:55 +0300

    update slave spi

--- platform/apollon_slave/cmds/test_spi_dma_send.c
+++ platform/apollon_slave/cmds/test_spi_dma_send.c
@@ -1,25 +1,40 @@
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
-
+  
+#include <stdint.h>
 #include "spi_gen/spi2_generated.h"
-#include <kernel/sched.h>
-#include <kernel/sched/waitq.h>
-#include <kernel/sched/schedee_priority.h>
 #include <kernel/lthread/lthread.h>
-#include <kernel/thread.h>
-#include <kernel/time/ktime.h>
+// #include <kernel/time/ktime.h>
 #include <kernel/lthread/sync/mutex.h>
 
 struct lthread WaitingTaskLth;
 
 static int WaitingRun(struct lthread *self) {
-    while(! SPI2_FULL_DMA_wait_rx_data())
+    struct mutex trg_m;
+    uint8_t result = 0;
+    goto *lthread_resume(self, &&start);
+start:
+    /* инициализация */
+    result = 0;
+    trg_m = SPI2_FULL_DMA_wait_rx_data();
+mutex_retry:
+    if (mutex_trylock_lthread(self, &trg_m ) == -EAGAIN)
     {
-
+        return lthread_yield(&&start, &&mutex_retry);
     }
-    printf("Receive data: done\n");
-    return 0;
+    if (SPI2_FULL_DMA_is_full())
+    {
+    	mutex_unlock_lthread(self, &trg_m);
+        printf("Receive data: done\n");
+        return result;
+    }
+    else
+    {
+        return lthread_yield(&&start, &&mutex_retry);
+    }
+    
 }
 
 int main(int argc, char *argv[]) {
@@ -37,7 +52,7 @@ int main(int argc, char *argv[]) {
     printf("\n");
 
     sleep(1);
-    SPI2_FULL_DMA_transmit(data, datacount);
+    // SPI2_FULL_DMA_transmit(data, datacount);
 
 
     printf("Transmit: done\n");
@@ -45,3 +60,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
