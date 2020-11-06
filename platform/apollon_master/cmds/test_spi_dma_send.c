aa494ab78fba03459f025a85412a95286097ad21
Artem <artem.o.kuznecov@gmail.com>
2020-11-06 17:15:01 +0300

    check some spi

--- platform/apollon_master/cmds/test_spi_dma_send.c
+++ platform/apollon_master/cmds/test_spi_dma_send.c
@@ -2,7 +2,8 @@
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
-
+  
+#include <stdint.h>
 #include "spi_gen/spi2_generated.h"
 #include <kernel/lthread/lthread.h>
 // #include <kernel/time/ktime.h>
@@ -11,10 +12,13 @@
 struct lthread WaitingTaskLth;
 
 static int WaitingRun(struct lthread *self) {
+    struct mutex trg_m;
+    uint8_t result = 0;
     goto *lthread_resume(self, &&start);
 start:
     /* инициализация */
-    struct mutex trg_m = SPI2_FULL_DMA_wait_rx_data();
+    result = 0;
+    trg_m = SPI2_FULL_DMA_wait_rx_data();
 mutex_retry:
     if (mutex_trylock_lthread(self, &trg_m ) == -EAGAIN)
     {
@@ -24,7 +28,7 @@ mutex_retry:
     {
     	mutex_unlock_lthread(self, &trg_m);
         printf("Receive data: done\n");
-        return 0;
+        return result;
     }
     else
     {
