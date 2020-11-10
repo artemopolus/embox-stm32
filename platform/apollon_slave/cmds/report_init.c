06a93220a5eec38e63ba094b0de3b8b97a86df0b
Artem <artem.o.kuznecov@gmail.com>
2020-11-10 17:28:10 +0300

    update reporter

---
+++ platform/apollon_slave/cmds/report_init.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>  
+#include <stdint.h>
+
+
+int main(int argc, char *argv[]) {
+    printf("This is slave 0.1\n");
+    return 0;
+}
