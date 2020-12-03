8d32357417dbd5aba0e8742613f105663b8dc1ad
Artem Kuznetcov <artem.o.kuznecov@gmail.com>
2020-12-03 09:47:47 +0300

    correct data

--- platform/apollon_slave/exacto_commander/exacto_data_storage.c
+++ platform/apollon_slave/exacto_commander/exacto_data_storage.c
@@ -1,12 +1,7 @@
 #include "exacto_data_storage.h"
-#include "project/base_project_defs.h"
-#include <errno.h>
 #include <embox/unit.h>
-#include <kernel/irq.h>
-#include <kernel/lthread/lthread.h>
-#include <kernel/lthread/sync/mutex.h>
 
-#define CHECK_ID    if (id >= MAIL_ID_CNT_EXACTO_COMMANDER )    return 1;
+#define CHECK_ID_THREAD_CONTROL(ID)    if (ID >= THREAD_CONTROL_CNT )    return 1;
 
 typedef struct{
     uint8_t isEmpty;
@@ -17,107 +12,111 @@ static exactodatastorage ExDtStorage = {
     .isEmpty = 0,
 };
 
-static uint8_t CntIDMail4Thread = 0;
+typedef struct{
+    uint8_t id_count;
+    uint8_t id_max;
+    struct mutex mx;
+}data_controller_t;
 
-typedef struct mail4lthread_t{
-    struct lthread thread;
-    uint8_t data[MAIL_SZ_EXACTO_COMMANDER];
-    uint8_t datalen;
-    uint8_t data2copylen;
-    uint8_t result; // 0 - not used 1 - waiting 2 - working
-}mail4lthread;
+static uint8_t DataControllerIDCnt = 0;
 
-static mail4lthread AppenderData[MAIL_ID_CNT_EXACTO_COMMANDER];
-static mail4lthread GetterData[GETT_ID_CNT_EXACTO_COMMANDER];
+typedef enum{
+    WAIT = 0,
+    UNKNOWN_ERROR,
+    NO_RESULT = 0xFF
+}thread_control_result_t;
 
-static int appenderData_handler(struct lthread *self)
+
+//LIST OF FUNCTONS
+
+void appendThreadControl(thread_control_t * thread);
+void getThreadControl(thread_control_t * thread);
+void checkThreadControl(thread_control_t * thread);
+
+
+static thread_control_t DataController[THREAD_CONTROL_CNT];
+
+// LIST OF THREADS
+
+static int operateDataController(struct lthread * self)
 {
+    return 0;
+}
+
+static int functionForExDtStorageHandler(struct lthread *self)
+{
+    thread_control_t *_trg_lthread;
     goto *lthread_resume(self, &&start);
-    mail4lthread *_trg_lthread;
 start:
-//     /* инициализация */
-//     result = 0;
+     /* инициализация */
+
 mutex_retry:
+    // do       something
+    if (mutex_trylock_lthread(self, &_trg_lthread->mx ) == -EAGAIN)
+    {
+        return lthread_yield(&&start, &&mutex_retry);
+    }
+    //===============================================================
     if (mutex_trylock_lthread(self, &ExDtStorage.dtmutex ) == -EAGAIN)
     {
         return lthread_yield(&&start, &&mutex_retry);
     }
-    // do something
-    _trg_lthread = (mail4lthread*)&self;
-    _trg_lthread->result = 0;
+    _trg_lthread = (thread_control*)&self;
+    _trg_lthread->result = NO_RESULT;
+    switch (_trg_lthread->fun_type)
+    {
+    case APPEND:
+        /* code */
+        break;
+    case GET:
+        break;
+    case CHECK:
+        break;
+    default:
+        break;
+    }
+
     mutex_unlock_lthread(self, &ExDtStorage.dtmutex);
+    //===============================================================
+    mutex_unlock_lthread(self, &_trg_lthread->mx);
+
+    // after    something
     return 0;
 }
 
-static int getterData_handler(struct lthread *self)
-{
-    goto *lthread_resume(self, &&start);
-start:
-//     /* инициализация */
-//     result = 0;
-mutex_retry:
-    if (mutex_trylock_lthread(self, &ExDtStorage.dtmutex ) == -EAGAIN)
-    {
-        return lthread_yield(&&start, &&mutex_retry);
-    }
-    // do something
-    mutex_unlock_lthread(self, &ExDtStorage.dtmutex);
-    return 0;
-}
+
 
 EMBOX_UNIT_INIT(initExactoDataStorage);
 static int initExactoDataStorage(void)
 {
     mutex_init_schedee(&ExDtStorage.dtmutex);
-    for (uint8_t i = 0; i < MAIL_ID_CNT_EXACTO_COMMANDER; i++)
+    for (uint8_t i = 0; i < THREAD_CONTROL_CNT; i++)
     {
-        AppenderData[i].datalen = 0;
-        AppenderData[i].data2copylen = 0;
-        AppenderData[i].result = 0xFF;
-        lthread_init(&AppenderData[i].thread, &appenderData_handler);
+        lthread_init(&DataController[i].thread, &functionForExDtStorageHandler);
     }
-    for (uint8_t i = 0; i < GETT_ID_CNT_EXACTO_COMMANDER; i++)
-    {
-        GetterData[i].datalen = 0;
-        GetterData[i].data2copylen = 0;
-        GetterData[i].result = 0xFF;
-        lthread_init(&GetterData[i].thread, &getterData_handler);
-
-    }
-    
+   
     // lthread_init(&GetterData.thread, &getterData_handler);
     return 0;
 }
 /* главный косяк тут в том, что запросить ID это необязательное мероприятие. Его можно миновать и получить жесть в работе */
 uint8_t addAppenderExactoDataStorage(void)
 {
-    if (CntIDMail4Thread >= MAIL_ID_CNT_EXACTO_COMMANDER)
-        return 0xFF;
-    uint8_t res = CntIDMail4Thread;
-    CntIDMail4Thread++;
+    uint8_t res = DataControllerIDCnt;
+    DataControllerIDCnt++;
     return res;
 }
 uint8_t appendDataToExactoDataStorage( const uint8_t id, uint8_t * data, const uint8_t datacount)
 {
-    CHECK_ID
+    CHECK_ID_THREAD_CONTROL(id)
     // copy here in storage
-    AppenderData[id].result = 0xFF;
-    lthread_launch(&AppenderData[id].thread);
+    lthread_launch(&DataController[i].thread);
     return 0;
 }
 uint8_t getDataFromExactoDataStorage( const uint8_t id, uint8_t * data, const uint8_t datacount)
 {
-    CHECK_ID
     return 0;
 }
 uint8_t checkExactoDataStorage( const uint8_t id)
 {
-    CHECK_ID
-    uint8_t res = 0;
-    GetterData[id].result = 0xFF;
-    GetterData[id].data2copylen = 0;
-    lthread_launch(&GetterData[id].thread);
-    lthread_join(&GetterData[id].thread);
-    res = GetterData[id].result;
     return res;
 }
--- platform/apollon_slave/exacto_commander/exacto_data_storage.h
+++ platform/apollon_slave/exacto_commander/exacto_data_storage.h
@@ -1,6 +1,26 @@
 #ifndef EXACTO_DATA_STORAGE_H
 #define EXACTO_DATA_STORAGE_H
+#include <errno.h>
+#include <kernel/irq.h>
+#include <kernel/lthread/lthread.h>
+#include <kernel/lthread/sync/mutex.h>
+
 #include <stdint.h>
+#include "project/base_project_defs.h"
+typedef enum{
+    APPEND = 0,
+    GET,
+    CHECK
+}function_list_t;
+// THREADS
+typedef struct {
+    struct lthread thread;                          // поток исполнения запросов
+    struct mutex mx;                             // контрольный мьютекс для контроля окончания потока
+    uint8_t databuffer[THREAD_CONTROL_BUFFER_SZ];   // буффер хранения данных
+    uint8_t datalen;
+    thread_control_result_t result;
+    function_list_t fun_type;
+}thread_control_t;
 extern uint8_t appendDataToExactoDataStorage( const uint8_t id, uint8_t * data, const uint8_t datacount);
 extern uint8_t getDataFromExactoDataStorage( const uint8_t id, uint8_t * data, const uint8_t datacount);
 extern uint8_t checkExactoDataStorage( const uint8_t id);
