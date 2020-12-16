9f124f8a3cd85767b099fbbfce5e47e94b74235a
Artem <artem.o.kuznecov@gmail.com>
2020-12-16 13:43:28 +0300

    set cnn model base

---
+++ platform/afina/tensorflow/CNN_model_tfmodel.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+#include "tensorflow/lite/micro/kernels/micro_ops.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/version.h"
+
+#include "tensorflow/CNN_model_tfmodel.h"
+
+// TFLite globals
+namespace {
+  tflite::ErrorReporter* error_reporter = nullptr;
+  const tflite::Model* model = nullptr;
+  tflite::MicroInterpreter* interpreter = nullptr;
+  TfLiteTensor* model_input = nullptr;
+  TfLiteTensor* model_output = nullptr;
+
+  // Create an area of memory to use for input, output, and other TensorFlow
+  // arrays. You'll need to adjust this by compiling, running, and looking
+  // for errors.
+  constexpr int kTensorArenaSize = 2 * 1024;
+  __attribute__((aligned(16)))uint8_t tensor_arena[kTensorArenaSize];
+} // namespace
+
